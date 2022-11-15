64452f13debe0d51168292e2882e02375a0872a6
Gonçalo Telo <gtelo@plux.info>
2022-11-15 13:49:42 +0000

    Initial Commit

---
+++ ios-sample/ios_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_sampleApp.swift
+//  ios-sample
+//
+//  Created by Gonçalo Telo on 15/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ios_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
